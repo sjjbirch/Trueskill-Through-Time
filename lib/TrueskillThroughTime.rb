d9906668a93536d34e04688c56dd3de8e6e7cde8
Solomon Birch <birch.jj@gmail.com>
2026-04-16 16:59:36 +1000

    Initial commit.

---
+++ lib/TrueskillThroughTime.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "TrueskillThroughTime/version"
+
+module TrueskillThroughTime
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_TrueskillThroughTime.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestTrueskillThroughTime < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::TrueskillThroughTime::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
